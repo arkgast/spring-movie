ea151499328490145ed717f1c5198d9841a8d502
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-11-04 14:09:55 -0400

    feat: setup clients to register to discovery server

--- catalog-service/src/main/java/org/veloud/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/org/veloud/catalogservice/CatalogServiceApplication.java
@@ -2,12 +2,13 @@ package org.veloud.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CatalogServiceApplication {
-
     @Bean
     public WebClient.Builder getWebClientBuilder() {
         return WebClient.builder();
@@ -16,5 +17,4 @@ public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
-
 }
--- info-service/src/main/java/org/veloud/infoservice/InfoServiceApplication.java
+++ info-service/src/main/java/org/veloud/infoservice/InfoServiceApplication.java
@@ -2,12 +2,12 @@ package org.veloud.infoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InfoServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InfoServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InfoServiceApplication.class, args);
+    }
 }
